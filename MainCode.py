e79623cc179f51fb4a2bf9668cbeace145d42a1b
GermanFiaCalderon <147556707+GermanFiaCalderon@users.noreply.github.com>
2023-10-16 11:20:38 -0600

    Add files via upload

--- MainCode.py
+++ MainCode.py
@@ -6,51 +6,61 @@ import RPi.GPIO as GPIO
 import sqlite3
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.OUT)
-
+#asigna la variable de la pantalla lcd
 pantallalcd = I2C_LCD_driver.lcd()
+#selecciona el tipo de sensor
 DHT_SENSOR = Adafruit_DHT.DHT11
+#selecciona el pin del sensor
 DHT_PIN = 4
-
+#hace la coneccion con la base de datos
 connection = sqlite3.connect("Data.db")
 print(connection.total_changes)
+#asigna la variable de configuracion con el sensor
 cursor = connection.cursor()
+#se encarga de limpiar la base de datos por si se encontraba sucia
 cursor.execute("DROP TABLE IF EXISTS data;")
 time.sleep(0.2)
 cursor.execute("CREATE TABLE data (Temp TEXT, Hume TEXT )")
-
+#abre el archivo de configuracion json
 with open("Config.json", "r") as archivo_json:
     datos_json = json.load(archivo_json)
 
 a = 1
 while a < 10:
+    #se consulta el sensor 
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
+        #se escribe en la pantalla lcd los datos
+        pantallalcd.lcd_clear()
         pantallalcd.lcd_display_string("Temp={0:0.1f}C ".format(temperature, humidity), 1)
         pantallalcd.lcd_display_string("Hume={1:0.1f}%".format(temperature, humidity), 2)
         #print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+        #el led se enciende con cada lectura
         GPIO.output(16,GPIO.HIGH)
         time.sleep(1.0)
         GPIO.output(16,GPIO.LOW)
-
+        #se escribe los datos en la base de datos
         cursor.execute("INSERT INTO data VALUES ('{0:0.1f}C', '{1:0.1f}%')".format(temperature, humidity))
       
         time.sleep(datos_json["tiempo"])
-
-        
+    #avisa si no se leyo el dato
     else:
-        print("Sensor failure. Check wiring.");
-        time.sleep(1);
-        
-    
-with open("Config.json", "r") as archivo_json:
-    datos_json = json.load(archivo_json)
+        print("Sensor failure. Check wiring.")
+        time.sleep(1)
 
-b = 1
-while b < 10:
-   humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-   if True:
-        cursor.execute("INSERT INTO data VALUES ('{0:0.1f}C', '{1:0.1f}%')".format(temperature, humidity))
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if True:
+       #escribe la base de datos en el terminal
         rows = cursor.execute("SELECT Temp, Hume FROM data").fetchall()
         print(rows)
+        pantallalcd.lcd_clear()
+        pantallalcd.lcd_display_string("Data Cleared", 1, 2)
+        #lipia la tabla
+        cursor.execute("DROP TABLE IF EXISTS data;")
+        time.sleep(0.2)
+        cursor.execute("CREATE TABLE data (Temp TEXT, Hume TEXT )")
         time.sleep(datos_json["send_data"])
-       
+
+     
+        
+    
--- sqlitetest.py
+++ sqlitetest.py
@@ -1,3 +1,4 @@
+import threading
 import Adafruit_DHT
 import json
 import I2C_LCD_driver
@@ -11,23 +12,24 @@ pantallalcd = I2C_LCD_driver.lcd()
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
 
-connection = sqlite3.connect("Data.db")
-print(connection.total_changes)
-cursor = connection.cursor()
-cursor.execute("DROP TABLE IF EXISTS data;")
-time.sleep(0.2)
-cursor.execute("CREATE TABLE data (Temp TEXT, Hume TEXT )")
+
 
 with open("Config.json", "r") as archivo_json:
     datos_json = json.load(archivo_json)
 
-while True:
- 
-    
-    a = 1
-    while a < 10:
+def infiniteloop1():
+    while True:
+        
+        connection = sqlite3.connect("Data.db", timeout=10)
+        print(connection.total_changes)
+        cursor = connection.cursor()
+        cursor.execute("DROP TABLE IF EXISTS data;")
+        time.sleep(0.2)
+        cursor.execute("CREATE TABLE data (Temp TEXT, Hume TEXT )")
+
         humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
         if humidity is not None and temperature is not None:
+            pantallalcd.lcd_clear()
             pantallalcd.lcd_display_string("Temp={0:0.1f}C ".format(temperature, humidity), 1)
             pantallalcd.lcd_display_string("Hume={1:0.1f}%".format(temperature, humidity), 2)
             #print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
@@ -36,25 +38,43 @@ while True:
             GPIO.output(16,GPIO.LOW)
 
             cursor.execute("INSERT INTO data VALUES ('{0:0.1f}C', '{1:0.1f}%')".format(temperature, humidity))
+            
       
             time.sleep(datos_json["tiempo"])
 
+    else:
+        print("Sensor failure. Check wiring.")
+        time.sleep(1)
+
+
+
+def infiniteloop2():
+    while True:
+        connection1 = sqlite3.connect("Data2.db", timeout=10)
+        cursor1 = connection1.cursor()
+        cursor1.execute("DROP TABLE IF EXISTS data;")
+        time.sleep(0.2)
+        cursor1.execute("CREATE TABLE data (Temp TEXT, Hume TEXT )")
+        humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+        cursor1.execute("INSERT INTO data VALUES ('{0:0.1f}C', '{1:0.1f}%')".format(temperature, humidity))
+
+        rows = cursor1.execute("SELECT Temp, Hume FROM data").fetchall()
         
-        else:
-            print("Sensor failure. Check wiring.");
-            time.sleep(1);
-           
-    
-
-
-    b = 1
-    while b < 10:
-       if True:
-            rows = cursor.execute("SELECT Temp, Hume FROM data").fetchall()
+        
+        if True:
+            
             print(rows)
+            pantallalcd.lcd_clear()
+            pantallalcd.lcd_display_string("Data Cleared", 1, 2)
+            time.sleep(2.0)
+            pantallalcd.lcd_clear()
+            
             time.sleep(datos_json["send_data"])
-            continue
+
         
-       
-        
-    
+
+thread1 = threading.Thread(target=infiniteloop1)
+thread1.start()
+
+thread2 = threading.Thread(target=infiniteloop2)
+thread2.start()
